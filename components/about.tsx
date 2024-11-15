4d6b4cdb0de8a41cc3e61bea0be5586c428a3ff7
YuginProgramming <dudaryev@gmail.com>
2024-11-15 08:41:36 +0200

    upd15

--- components/about.tsx
+++ components/about.tsx
@@ -20,7 +20,7 @@ export const About = () => {
                        className="flex items-center hover:underline transition">
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-2" fill="none"
                              viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" stroke-linejoin="round" strokeWidth="2"
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                   d="M12 4v16m8-8H4"/>
                         </svg>
                         Як це працює
