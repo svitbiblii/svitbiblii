f1f0dc25d9612d859be4a79cc23daf836960b319
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2024-11-19 14:23:28 +0200

    update HowItWorksPage

--- app/(how-it-works)/(routes)/how-it-works/page.tsx
+++ app/(how-it-works)/(routes)/how-it-works/page.tsx
@@ -4,7 +4,7 @@ const HowItWorksPage = () => {
         <div className="p-8 max-w-3xl mx-auto">
             {/* <BackButton /> */}
             <h1 className="text-3xl font-bold mt-4">Як це працює</h1>
-            <p className="text-lg mt-4 leading-relaxed text-gray-700">
+            <p className="mt-4 ">
             Тут має бути сторінка Як це працює, на якій буде розповідь як користуватися сайтом.
             На мою думку сюди було б чудово теж додати слайди, на яких буде показано, 
             що користувач може зробити на сайті. Ці слайди потрібно малювати саме під 
