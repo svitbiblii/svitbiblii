1220933dde8e0410077ce358adb81b0739398159
Marina <marikki@MacBook-Pro-Yegor.local>
2024-11-25 11:04:28 +0200

    remove hide sidebar on single page

--- app/(single-page)/layout.tsx
+++ app/(single-page)/layout.tsx
@@ -5,7 +5,7 @@ import {Navbar} from "@/components/navbar";
 import {About} from "@/components/about";
 import {Sidebar} from "@/components/sidebar";
 import {Int} from "@/app/(single-page)/components/int";
-import { ChevronFirst, ChevronLast} from "lucide-react";
+/*import { ChevronFirst, ChevronLast} from "lucide-react";*/
 import InterpretationPage from "@/app/(single-page)/(routes)/single-page/interpretation-page";
 
 const SinglePageLayout = ({
@@ -22,21 +22,29 @@ const SinglePageLayout = ({
         <div className="h-min-full">
             <Navbar/>
             <div className="flex">
-                <div className="relative">
+                {/*<div className="relative">
                     <button onClick={() => setExpanded(curr => !curr)}
                             className={`absolute top-24 z-20 ${expanded ? "left-60" : "left-8"} hidden md:block p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100`}>
                         {expanded ? <ChevronFirst/> : <ChevronLast/>}
                     </button>
-                </div>
+                </div>*/}
                 <div
-                    className={`hidden  h-screen w-72 min-w-72 mt-16 overflow-y-auto bg-white dark:bg-secondary pb-12 shadow-lg                    ${
+                    className={`hidden  h-screen w-72 min-w-72 mt-16 overflow-y-auto bg-white dark:bg-secondary pb-12 shadow-lg`}>
+                    <div>
+                        <About/>
+                        <Sidebar/>
+                    </div>
+                </div>
+                {/*<div
+                    className={`hidden  h-screen w-72 min-w-72 mt-16 overflow-y-auto bg-white dark:bg-secondary pb-12 shadow-lg
+                    ${
                         expanded ? "md:block" : "initial"
                     }`}>
                     <div>
                         <About/>
                         <Sidebar/>
                     </div>
-                </div>
+                </div>*/}
                 <div className="h-screen w-full mt-16 py-4 pl-4 shadow-lg">
                     <main>
                         <div className="w-full"><Int isOpen={isOpen} toggleDiv={toggleDiv}/></div>
