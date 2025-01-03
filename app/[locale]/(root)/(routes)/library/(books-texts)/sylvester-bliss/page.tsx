2299e891246a649288d27f8ab298c934d908e414
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2025-01-03 16:39:31 +0200

    update state

--- app/[locale]/(root)/(routes)/library/(books-texts)/sylvester-bliss/page.tsx
+++ app/[locale]/(root)/(routes)/library/(books-texts)/sylvester-bliss/page.tsx
@@ -8,6 +8,8 @@ import { ChevronDown, ChevronUp, CircleX } from "lucide-react";
 export default function SylvesterBlissPage() {
     const [showPage, setShowPage] = useState(false);
     const [expanded, setExpanded] = useState(false);
+    const [expanded1, setExpanded1] = useState(false);
+    const [expanded2, setExpanded2] = useState(false);
 
     const togglePage = () => {
         setShowPage(!showPage)
@@ -94,14 +96,14 @@ Nor does anything tend to deepen such perilous estrangement, more than the very 
                     <Link href='/library/chadwick/#scientific' className="text-blue-500">scientific
                         <span className="text-black ml-2 dark:text-white">in the book A Brief Commentary on the Apocalypse by Sylvester Bliss</span>
                     </Link>
-                    <button onClick={() => setExpanded(curr => !curr)}
+                    <button onClick={() => setExpanded1(curr => !curr)}
                             className="">
-                        {expanded ? <ChevronUp/> : <ChevronDown/>}
+                        {expanded1 ? <ChevronUp/> : <ChevronDown/>}
                     </button>
                 </div>
                
                     <p className={`hidden  text-gray-700 mt-5 ${
-                        expanded ? "md:block" : "initial"
+                        expanded1 ? "md:block" : "initial"
                     }`}>
                         The Expositor&apos;s Bible: The Book of Exodus by G. A. Chadwick is a theological work written in the late 19th century. This text is an exposition on the Book of Exodus from the Old Testament, aiming to explore its spiritual significance and connection to Christianity, presenting an interpretation of the events and characters found within. The author, a notable church leader, seeks to provide insights that are both educational and edifying, reflecting his belief in the enduring authority of biblical scripture. At the start of this volume, the introduction presents the context of the Book of Exodus, discussing the transition from personal stories of the patriarchs to the national narrative of Israel. The preface establishes the significance of Exodus, emphasizing its impact on the Hebrew nation and the underlying spiritual lessons that extend beyond its historical events. The opening sections highlight the oppression of the Israelites in Egypt and introduces Moses, whose birth occurs amid a climate of fear and tyranny. Chadwick elaborates on Moses&#39; early life, the influence of his parents, his upbringing in Pharaoh&#39;s household, and the formative choices that shape his identity and destiny, setting the stage for the significant events that will follow in the narrative.</p>
                 
@@ -111,17 +113,16 @@ Nor does anything tend to deepen such perilous estrangement, more than the very 
                     <Link href='/library/john/#conjunction' className="text-blue-500">conjunction
                         <span className="text-black ml-2 dark:text-white">in the book The Epistles by St. John</span>
                     </Link>
-                    <button onClick={() => setExpanded(curr => !curr)}
+                    <button onClick={() => setExpanded2(curr => !curr)}
                             className="">
-                        {expanded ? <ChevronUp/> : <ChevronDown/>}
+                        {expanded2 ? <ChevronUp/> : <ChevronDown/>}
                     </button>
                 </div>
                
                     <p className={`hidden  text-gray-700 mt-5 ${
-                        expanded ? "md:block" : "initial"
+                        expanded2 ? "md:block" : "initial"
                     }`}>
                         The Epistles of St. John by William Alexander is a theological exposition written in the late 19th century. This work is part of a series that aims to provide in-depth commentary and analysis of Biblical texts, specifically focusing on the epistles attributed to St. John. It explores the historical context, theological significance, and practical implications of these letters, which are crucial for understanding early Christian doctrine and ethics. The opening of the text presents a preface by the author, explaining his long-standing engagement with the Epistles of St. John. He outlines his approach, which combines historical background with exegetical analysis, framing the epistles within the broader context of St. John&#39;s life and the challenges faced by the early Church in Asia Minor, particularly Ephesus. The author prepares the reader for a detailed examination of the epistles, highlighting the importance of understanding the unique spiritual and theological insights that St. John offers, especially against the backdrop of contemporary heresies such as Gnosticism. The opening sets the stage for a scholarly yet accessible exploration intended for both serious theological study and general readers interested in the New Testament.</p>
-                
             </li>            
             </ul>
         </div> }
