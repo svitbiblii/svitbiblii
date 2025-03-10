bae1c67aacb5580fffb3bd14a95b9016199b4fa9
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2025-03-10 14:13:17 +0200

    update

--- app/[locale]/(root)/layout.tsx
+++ app/[locale]/(root)/layout.tsx
@@ -1,36 +1,3 @@
-// "use client";
-
-// import { Navbar } from "@/components/navbar";
-// import { Footer } from "@/components/footer";
-// import { useRef } from "react";
-// import ScrollToTopButton from "@/components/ScrollToTopButton";
-
-// const HomeLayout = ({
-//                         children
-//                     }: {
-//     children: React.ReactNode;
-// }) => {
-//     const scrollContainerRef = useRef<HTMLDivElement>(null);
-
-//     return (
-//         <div className="h-min-full">
-//             <Navbar />
-//             <div className="flex">
-//                 <div ref={scrollContainerRef} className="h-screen w-full mt-16 overflow-y-auto shadow-lg">
-//                     <main>
-//                         {children}
-//                         <Footer/>
-//                     </main>
-//                     <ScrollToTopButton scrollContainerRef={scrollContainerRef} />
-//                 </div>
-//             </div>
-
-//         </div>
-//     );
-// };
-
-// export default HomeLayout;
-
 "use client";
 
 import { useState } from "react";
@@ -55,24 +22,6 @@ return (
     <div className="h-min-full">
 
         <Navbar />
-       
-            {/* <div className="relative">
-                <button onClick={() => setExpanded(curr => !curr)}
-                        className={`absolute top-24 z-20 ${expanded ? "left-60 dark:bg-secondary" : "left-8 dark:bg-background"} hidden md:block p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100 dark:color-white`}>
-                    {expanded ? <ChevronFirst/> : <ChevronLast/>}
-                </button>
-            </div>
-
-            <div
-                className={`hidden  h-screen w-72 min-w-72 mt-16 overflow-y-auto bg-white dark:bg-secondary pb-12 shadow-lg                    ${
-                    expanded ? "md:block" : "initial"
-                }`}>
-                    <div>
-                        <About/>
-                        <Sidebar/>
-
-                    </div>
-            </div> */}
 
                 <div ref={scrollContainerRef} className="h-screen w-full overflow-y-auto shadow-lg">
                     <main>
@@ -81,8 +30,6 @@ return (
                     </main>
                     <ScrollToTopButton scrollContainerRef={scrollContainerRef} />
                 </div>
-
-            
 
         </div>
     );
