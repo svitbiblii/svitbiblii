7263ef508df608165cc2269f464100a3e3f3a56a
Marina <marikki@MacBook-Pro-Yegor.local>
2025-03-11 15:02:07 +0200

    categories on library

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,10 +1,10 @@
 export const CATEGORIES = [
     { id: "all", name: "Всі" },
-    { id: "kat1", name: "Категория 1" },
-    { id: "kat2", name: "Категория 2" },
-    { id: "kat3", name: "Категория 3" },
-    { id: "kat4", name: "Категория 4" },
-    { id: "kat5", name: "Категория 5" },
-    { id: "kat6", name: "Категория 6" },
-    { id: "kat7", name: "Категория 7" },
+    { id: "kat1", name: "Категорія 1" },
+    { id: "kat2", name: "Категорія 2" },
+    { id: "kat3", name: "Категорія 3" },
+    { id: "kat4", name: "Категорія 4" },
+    { id: "kat5", name: "Категорія 5" },
+    { id: "kat6", name: "Категорія 6" },
+    { id: "kat7", name: "Категорія 7" },
 ];
