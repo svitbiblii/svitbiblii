4e68029249bae5172b648bd74e15d6d8fc96499f
Michael Plaksyuk <m.plaksyuk@gmail.com>
2025-07-04 11:51:05 +0300

    clerk auth

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -1,17 +1,16 @@
 import { ReactNode } from 'react';
 import { PageProvider } from '@/context/PageContext';
-import { UserProvider } from '@/context/UserContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
-		<UserProvider>
-			<PageProvider>
-				<Header />
-				{children}
-				<Footer />
-			</PageProvider>
-		</UserProvider>
+		// <UserProvider>
+		<PageProvider>
+			<Header />
+			{children}
+			<Footer />
+		</PageProvider>
+		// </UserProvider>
 	);
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from '../../components/theme-provider';
 import { NextIntlClientProvider } from 'next-intl';
 
 import { getMessages } from 'next-intl/server';
+import { ClerkProvider } from '@clerk/nextjs';
 
 export default async function Layout({
 	children,
@@ -16,12 +17,16 @@ export default async function Layout({
 	const messages = await getMessages();
 
 	return (
-		<html lang={locale} suppressHydrationWarning>
-			<body className="flex flex-col min-h-screen">
-				<ThemeProvider>
-					<NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>
-				</ThemeProvider>
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang={locale} suppressHydrationWarning>
+				<body className="flex flex-col min-h-screen">
+					<ThemeProvider>
+						<NextIntlClientProvider messages={messages}>
+							{children}
+						</NextIntlClientProvider>
+					</ThemeProvider>
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
