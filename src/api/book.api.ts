6107e6676b1709b87bd15be8db6ac4553ffb3b90
Michael Plaksyuk <m.plaksyuk@gmail.com>
2025-07-06 01:46:24 +0300

    fix method name

--- src/api/book.api.ts
+++ src/api/book.api.ts
@@ -2,8 +2,8 @@ import api from '@/api/axios';
 import { BookDTO, BookSearchParams } from '@/types/book.dto';
 
 const bookAPI = {
-	get: (book_id: BookDTO['id']) => api.get<BookDTO>(`/auth/book/${book_id}`),
-	gatAll: (params: BookSearchParams) => api.get<BookDTO[]>('/auth/book/', { params }),
+	get: (book_id: BookDTO['id']) => api.get<BookDTO>(`/book/${book_id}`),
+	getAll: (params: BookSearchParams) => api.get<BookDTO[]>('/book/', { params }),
 };
 
 export default bookAPI;
