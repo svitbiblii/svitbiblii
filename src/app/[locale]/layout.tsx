c1988fc4b87a6b90fa4fcf9b335d06a1c899056b
Michael Plaksyuk <m.plaksyuk@gmail.com>
2025-07-06 01:45:21 +0300

    move layout. fix locale switcher

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,11 @@
 import '@/src/app/globals.css';
-
 import { ReactNode } from 'react';
-import { ThemeProvider } from '../../components/theme-provider';
 import { NextIntlClientProvider } from 'next-intl';
 
 import { getMessages } from 'next-intl/server';
+import { routing } from '@/i18n/routing';
+import { notFound } from 'next/navigation';
+import { ThemeProvider } from 'next-themes';
 import { ClerkProvider } from '@clerk/nextjs';
 
 export default async function Layout({
@@ -14,14 +15,18 @@ export default async function Layout({
 	children: ReactNode;
 	params: { locale: string };
 }) {
-	const messages = await getMessages();
+	if (!routing.locales.includes(locale as (typeof routing.locales)[number])) {
+		notFound();
+	}
+
+	const messages = await getMessages({ locale });
 
 	return (
 		<ClerkProvider>
 			<html lang={locale} suppressHydrationWarning>
 				<body className="flex flex-col min-h-screen">
 					<ThemeProvider>
-						<NextIntlClientProvider messages={messages}>
+						<NextIntlClientProvider locale={locale} messages={messages}>
 							{children}
 						</NextIntlClientProvider>
 					</ThemeProvider>
